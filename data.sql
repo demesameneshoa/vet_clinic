51c1b90af4f7b0875829b5c7b32c0b45b80126a1
demesameneshoa <demesameneshoa@gmail.com>
2023-08-16 23:16:28 +0300

    inserted owners and species details and link with animals table

--- data.sql
+++ data.sql
@@ -14,3 +14,31 @@ VALUES
 ('Boarmon','2005-06-07',7,true,20.4),
 ('Blossom','1998-10-13',3,true,17),
 ('Ditto','2022-05-14',4,true,22);
+--Insert owners data
+INSERT INTO owners (full_name,age)
+VALUES
+('Sam Smith',34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+-- insert species data
+INSERT INTO species (name)
+VALUES
+('Pokemon'),
+('Digimon');
+
+--Update species id as per the requirement
+UPDATE animals SET species_id=2 WHERE name LIKE '%mon';
+UPDATE animals SET species_id=1 WHERE species_id IS NULL;
+-- update owners id
+BEGIN;
+UPDATE animals SET owner_id=1 WHERE name='Agumon';
+UPDATE animals SET owner_id=2 WHERE name='Gabumon' OR name='Pikachu';
+UPDATE animals SET owner_id=3 WHERE name='Devimon' OR name='Plantmon';
+UPDATE animals SET owner_id=4 WHERE name='Charmander' OR name='Squirtle' OR name='Blossom';
+UPDATE animals SET owner_id=5 WHERE name='Angemon' OR name='Boarmon';
+SELECT * FROM animals;
+COMMIT;
+
