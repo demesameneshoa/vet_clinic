3aeda058a992795123b4be400ae6b5eb5a123080
demesameneshoa <demesameneshoa@gmail.com>
2023-08-16 21:45:30 +0300

    Create species and owners table

--- schema.sql
+++ schema.sql
@@ -9,3 +9,16 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species varchar(100);
+
+CREATE TABLE owners (
+    id Int GENERATED ALWAYS AS IDENTITY,
+    full_name varchar(200),
+    age Int,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE species (
+    id Int GENERATED ALWAYS AS IDENTITY,
+    name varchar(200),
+    PRIMARY KEY (id)
+);
